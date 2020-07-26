3d2efd8b2fe8d6e8440ff9a757db67f8f4cb2c85
Julia Clements <tiiedye@gmail.com>
2020-07-25 17:53:43 -0700

    Updates

---
+++ precache-manifest.ccd7e09af0254654133081bcbdac3f29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04364cedb2c4dbe1395e509d10ebceb",
+    "url": "/ThirdProjectDev/index.html"
+  },
+  {
+    "revision": "e1bbdc8dcd7aa37767cb",
+    "url": "/ThirdProjectDev/static/css/main.360d5f89.chunk.css"
+  },
+  {
+    "revision": "c28f8e23164e0b3e050c",
+    "url": "/ThirdProjectDev/static/js/2.85331387.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ThirdProjectDev/static/js/2.85331387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bbdc8dcd7aa37767cb",
+    "url": "/ThirdProjectDev/static/js/main.7cb99dc6.chunk.js"
+  },
+  {
+    "revision": "3538afb6536a948aafbb",
+    "url": "/ThirdProjectDev/static/js/runtime-main.a4e5f18c.js"
+  },
+  {
+    "revision": "9c7f1dc0e7be054008ffd2dfd8322948",
+    "url": "/ThirdProjectDev/static/media/adventure-home.9c7f1dc0.png"
+  },
+  {
+    "revision": "84d6bdac163656fe5e6830b7be076b7b",
+    "url": "/ThirdProjectDev/static/media/tpd-banner.84d6bdac.png"
+  }
+]);
