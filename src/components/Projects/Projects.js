98539a2c3d4c221ad15c586d72593f36e8da252f
AnnaxGrace <anna.grace.conover@gmail.com>
2020-11-10 12:48:09 -0600

    Updated with Along with me

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,17 @@ const projectMap = [
         img: require("../../assets/adventure-home.png"),
         description: "Once Upon an Adventure is an interactive learning experience. Based on both RPG's and Choose Your Own Adventure games, Once Upon an Adventure offers a variety of learning games that any child will enjoy.",
         deployed: "https://onceuponanadventure.herokuapp.com/",
-        repo: "https://github.com/annaxgrace/once-upon-an-adventure"
+        repo: "https://github.com/annaxgrace/once-upon-an-adventure",
+        construction: false,
+    },
+    {
+        id: 2,
+        name: "Along With Me",
+        img: require("../../assets/alongwithme.png"),
+        description: "This audiobook companion app takes the text of your audiobook and checks for specific words for background noise. Really transport yourself! When our application registers the word 'fire' hear a crackling fireplace. Set the specific sounds you want to hear and the volume in the settings.",
+        deployed: "https://tiiedye.github.io/ThirdProjectDev/UnderConstruction",
+        repo: "https://github.com/eh4git/Along-With-Me",
+        construction: true
     }
 ]
 
@@ -36,6 +46,12 @@ function Projects() {
                             <Button>Repository</Button>
                         </a>
                     </ButtonGroup>
+                    {item.construction ? (
+                        <p>
+                            Currently under construction
+                        </p>
+                    ) : ""}
+                    <hr/>
                 </div>
             ))}
         </div>
